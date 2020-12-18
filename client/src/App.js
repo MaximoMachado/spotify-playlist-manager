9d605750810fd903988b2f3510630502ab8430d5
Maximo Machado <maximo@mit.edu>
2020-12-18 16:15:25 -0500

    Create 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage';
+import { NotFound } from './pages/NotFound/NotFound';
 
 function App() {
     return (
         <Router>
             <Switch>
-                <Route path='/'><HomePage /></Route>
+                <Route exact path='/'><HomePage /></Route>
+                
+                <Route path='*'><NotFound /></Route>
             </Switch>
         </Router>
     );
---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import './NotFound.css';
+
+function NotFound() {
+    return (
+        <div>
+            <h1>Error 404. Page Not Found.</h1>
+        </div>
+    )
+}
+
+
+export { NotFound };
