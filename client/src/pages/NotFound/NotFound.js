fac42d7fdf2b7edc2a711fc3f3a6d76be0506f14
Maximo Machado <maximo@mit.edu>
2020-12-18 17:04:22 -0500

    Design and create functional 404 page

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,10 +1,31 @@
 import './NotFound.css';
+import { Flex, Spacer, Heading, Text, Button } from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
 
 function NotFound() {
+    let history = useHistory();
+
     return (
-        <div>
-            <h1>Error 404. Page Not Found.</h1>
-        </div>
+        <Flex 
+            justify='center' 
+            align='center' 
+            width='100vw' 
+            height='100vh'
+            background='#EBF8FF'
+        >
+            <Flex direction='column' align='center' height='25%'>
+                <Heading>Error 404</Heading>
+                <Text>Sorry! The page you were looking for doesn't exist.</Text>
+
+                <Spacer />
+                <Button
+                    boxShadow='md'
+                    onClick={() => history.goBack()}
+                >
+                    Return to Previous Page
+                </Button>
+            </Flex>
+        </Flex>
     )
 }
 
