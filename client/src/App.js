5d418de579a5cb5779bc4f82e318acd9b867b13b
Maximo Machado <maximo@mit.edu>
2020-12-18 16:01:32 -0500

    Import React Router and implement basic functionality into app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route path='/'><HomePage /></Route>
+            </Switch>
+        </Router>
     );
 }
 export default App;
