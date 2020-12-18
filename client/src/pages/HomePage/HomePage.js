5e51e68dda5e19383790a8a8e3c189b0d6666527
Maximo Machado <maximo@mit.edu>
2020-12-18 18:17:02 -0500

    Create homepage without working login button

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,12 +1,39 @@
 import './HomePage.css';
+import { Link, Flex, Spacer, Heading, Text, Button } from '@chakra-ui/react';
+import {useHistory} from 'react-router-dom';
 
 function HomePage() {
-    return (
-        <div>
-            <h1>This is a homepage.</h1>
-        </div>
-    )
-}
+    let history = useHistory();
 
+    return (
+        <Flex
+            direction='column'
+            justify='center'
+            align='center'
+            width='100vw' 
+            height='100vh'
+            background='#EBF8FF'
+        >
+            <Text visibility='hidden' marginBottom='auto' align='center'></Text>
+            <Flex direction='column' align='center' height='25%'>
+                <Heading align='center'>Spotify Playlist Manager</Heading>
+                <Spacer />
+                <Text align='center'>A collection of different tools related to playlist management and creation.</Text>
+                <Spacer />
+                <Text align='center'>Some examples include checking if a song is in saved playlists and set operations on playlists.</Text>
+                <Spacer />
+                <Button
+                    boxShadow='md'
+                    onClick={() => history.goBack()}
+                >
+                    Log In With Spotify
+                </Button>
+            </Flex>
+            <Text marginTop='auto' paddingBottom='1em' align='center'> 
+                Maximo Machado | maximo@mit.edu | <Link textDecoration='underline' isExternal href='https://github.com/MaximoMachado/spotify-playlist-manager'>Github Repo</Link> 
+            </Text>
+        </Flex>
+    );
+}
 
 export {HomePage};
