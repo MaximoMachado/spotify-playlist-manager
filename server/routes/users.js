52026e44532650b66cfbfaca69608d73bbbbee70
Maximo Machado <maximo@mit.edu>
2020-12-19 14:04:23 -0500

    Modify users route to make login request to spotify

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+    const scopes = 'playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative';
+    const redirect_uri = 'https://localhost:3000/tools';
+
+    res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}` +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + encodeURIComponent(redirect_uri));
 });
 
 module.exports = router;
