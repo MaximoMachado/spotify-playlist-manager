98c35dacc3cfd253192fff7e1112eb6488d51c4f
Maximo Machado <maximo@mit.edu>
2020-12-19 19:23:32 -0500

    Move redirect uri to env file

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/login', function(req, res, next) {
     const scopes = 'playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative';
-    const redirect_uri = 'https://localhost:3000/tools';
 
     res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}` +
     (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-    '&redirect_uri=' + encodeURIComponent(redirect_uri));
+    '&redirect_uri=' + encodeURIComponent(process.env.REDIRECT_URI));
 });
 
 module.exports = router;
