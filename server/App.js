12bd0848599335f27c00dd2934796adeb7191643
Maximo Machado <maximo@mit.edu>
2020-12-19 20:13:00 -0500

    Import axios and write callback route for use with login

--- server/App.js
+++ server/App.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var axios = require('axios');
 
 var usersRouter = require('./routes/users');
 
@@ -13,7 +14,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Routes
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+
+
+// Callback to handle Spotify Login Request
+app.get('/api/callback', async (req, res) => {
+    const code = req.query.code || null;
+    const error = req.query.error || null;
+
+    if (code !== null) {
+        // User accepted permissions
+        const tokenRes = await axios.post('https://accounts.spotify.com/api/token', 
+            {
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: process.env.REDIRECT_URI,
+            },
+            { 
+                params: { 
+                    client_id: process.env.CLIENT_ID, 
+                    client_secret: process.env.CLIENT_SECRET 
+                } 
+            });
+
+
+    } else {
+        // Error
+
+        res.redirect(process.env.FRONT_END_URL);
+    }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.get('/login', function(req, res, next) {
     const scopes = 'playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative';
-
+    
+    // TODO Add a random string with state to prevent CSRF Attacks
     res.redirect(`https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}` +
     (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
     '&redirect_uri=' + encodeURIComponent(process.env.REDIRECT_URI));
