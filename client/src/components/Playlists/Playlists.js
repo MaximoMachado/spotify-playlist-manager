af25608c0947d7742a0dc56c2b391a5ed9b0da67
Maximo Machado <maximo@mit.edu>
2020-12-20 22:53:37 -0500

    Remove call to getUserPlaylists from Playlists to generalize component

---
+++ client/src/components/Playlists/Playlists.js
@@ -0,0 +1,29 @@
+import {useState, useEffect} from 'react';
+import {StyledVStack} from '../StyledVStack/StyledVStack';
+import { Playlist } from '../Playlist/Playlist';
+
+function Playlists({ playlists, fullInfo=true, ...style}) {
+
+    const [playlistCards, setPlaylistCards] = useState([]);
+
+    useEffect(() => {
+        if (playlists.data !== undefined) {
+            const items = playlists.data.body.items;
+            
+            setPlaylistCards(items.map(playlistData => {
+                return <Playlist 
+                    key={playlistData.uri}
+                    playlistData={playlistData}
+                />;
+            }))
+        }
+    }, [playlists])
+
+    return (
+        <StyledVStack {...style}>
+            {playlistCards}
+        </StyledVStack>
+    )
+}
+
+export {Playlists};
--- client/src/pages/MultiplePlaylistSearcher/MultiplePlaylistSearcher.js
+++ client/src/pages/MultiplePlaylistSearcher/MultiplePlaylistSearcher.js
@@ -1,11 +1,21 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import {PageLayout} from '../../components/PageLayout/PageLayout';
-import { UserPlaylists } from '../../components/UserPlaylists/UserPlaylists';
+import { Playlists } from '../../components/Playlists/Playlists';
 
 function MultiplePlaylistSearcher() {
 
+    const [playlists, setPlaylists] = useState({});
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_URL}/spotify/getUserPlaylists`, { withCredentials: true })
+            .then(data => setPlaylists(data))
+            .catch(err => console.error(err))
+    }, [])
+
     return (
         <PageLayout height='100%' width='100%'>
-            <UserPlaylists/>
+            <Playlists playlists={playlists}/>
         </PageLayout>
     )
 }
