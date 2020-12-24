633c4668558605022479d67536027fbe3ed4d3ec
Maximo Machado <maximo@mit.edu>
2020-12-24 16:34:17 -0500

    Install msw to client and add server to setupTests

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -4,3 +4,19 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import './test-utils';
+import { rest } from 'msw';
+import { setUpServer } from 'msw/node';
+
+const server = setUpServer(
+    rest.get('*', (req, res, ctx) => {
+        console.error(`Please add request handler for ${req.url}`);
+        return res(
+            ctx.status(500),
+            ctx.json({ error: 'Please add request handler'}),
+        );
+    })
+);
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
