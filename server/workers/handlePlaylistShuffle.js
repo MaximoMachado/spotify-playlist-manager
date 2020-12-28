61191d285eea7e7700a443cd35f3f92f82661d2d
Maximo Machado <maximo@mit.edu>
2020-12-28 00:19:00 -0500

    Implement worker that adds shuffled playlist to spotify user's account

--- server/workers/handlePlaylistShuffle.js
+++ server/workers/handlePlaylistShuffle.js
@@ -1,19 +1,31 @@
 var Queue = require('bull');
 var SpotifyWebApi = require('spotify-web-api-node');
+var { getPlaylistTracks } = require('../utils/getAll');
+var shuffleArray = require('../utils/shuffleArray');
 
 const handlePlaylistShuffle = new Queue('handle-playlist-shuffle');
 
 handlePlaylistShuffle.process(async (job) => {
     const { uri, accessToken } = job.data;
-
+    const playlistId = uri.split(':')[2];
     try {
         const spotifyApi = new SpotifyWebApi({ accessToken: accessToken });
 
-        const userRes = await spotifyApi.getMe();
-        const user = userRes.body;
+        const res = await spotifyApi.getMe();
+        const user = res.body;
 
-        const playlistRes = await spotifyApi.getPlaylist(uri);
+        const trackUris = [];
+        for await (let playlistTrack of getPlaylistTracks(playlistId, accessToken)) {
+            trackUris.push(playlistTrack.track.uri);
+        }
 
+        shuffleArray(trackUris);
+
+        const name = `SPM True Shuffle: ${playlistId}`;
+        const newPlaylistRes = await spotifyApi.createPlaylist(user.id, name);
+        const { newPlaylist } = newPlaylistRes.body;
+
+        spotifyApi.addTracksToPlaylist(newPlaylist.id, trackUris);
     } catch (err) {
         console.error(err);
     }
