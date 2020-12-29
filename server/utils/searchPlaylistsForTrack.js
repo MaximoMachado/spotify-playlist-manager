babb9e414bf1161bf30063678459f766e3a97194
Maximo Machado <maximo@mit.edu>
2020-12-29 13:57:50 -0500

    Implement back-end handling of settings into MPS

--- server/utils/searchPlaylistsForTrack.js
+++ server/utils/searchPlaylistsForTrack.js
@@ -1,13 +1,18 @@
 var { getPlaylistTracks, getUserPlaylists } = require('./getAll');
 
-async function* searchPlaylistsForTrack(targetUri, accessToken) {
+async function* searchPlaylistsForTrack(targetUri, playlistsToExclude, accessToken) {
     /**
      * Yields playlists track is in
      * Params:
      * targetUri {str}: Uri of track searching for
+     * playlistsToExclude {Set}: Set of playlist uri's that will not be searched
      * accessToken {str}: Access Token provided by user authentication
      */
     for await (let playlist of getUserPlaylists(accessToken)) {
+        if (playlistsToExclude.has(playlist.uri)) {
+            continue;
+        }
+        
         for await (let playlistTrack of getPlaylistTracks(playlist.id, accessToken)) {
             let { track } = playlistTrack;
             
