e8ced2bdd34909d3743abd7c54124478c4e6e6f4
Maximo Machado <maximo@mit.edu>
2020-12-29 19:53:45 -0500

    Implement logger for bull queues that prints every hour

---
+++ server/workers/handleWorkerLogs.js
@@ -0,0 +1,30 @@
+var Queue = require('bull');
+var { handleUpdateQueue, insertDb, modifyDb, addPlaylistQueue } = require('./handleUpdate');
+var handlePlaylistShuffle = require('./handlePlaylistShuffle');
+
+const handleWorkerLogs = new Queue('queue-logger');
+
+handleWorkerLogs.process(async (job) => {
+    try {
+        const queues = {
+            'Handle Update': handleUpdateQueue, 
+            'Insert Db': insertDb, 
+            'Modify Db': modifyDb, 
+            'Add Playlist': addPlaylistQueue
+        };
+
+        let loggingText = '--- WORKER LOGS START ---\n';
+        loggingText += `Current Date and Time: ${new Date().toLocaleString()}\n`;
+
+        for (let [key, queue] of Object.entries(queues)) {
+            loggingText += `${key} Jobs:\n ${JSON.stringify(await queue.getJobCounts())}\n`;
+        }
+        console.log(loggingText);
+    } catch (err) {
+        console.error(err);
+    }
+
+});
+
+
+module.exports = { handleWorkerLogs };
