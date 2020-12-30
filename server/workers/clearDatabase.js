9010e5cc5b9552fbbf0e0017e36cb8bcc69ccb0e
Maximo Machado <maximo@mit.edu>
2020-12-29 20:42:55 -0500

    Unready all users when database cleared

--- server/workers/clearDatabase.js
+++ server/workers/clearDatabase.js
@@ -6,12 +6,12 @@ const clearDb = new Queue('clear-db');
 
 clearDb.process((job) => {
     /**
-     * Deletes all rows in track_in_playlist and user_saved_playlists in database.
+     * Deletes all rows in track_in_playlist and user_saved_playlists in database. Unreadies all users so database isn't used.
      * Should be run every so often to prevent database from growing too large.
      */
-
-     db.query('DELETE FROM public.track_in_playlist');
-     db.query('DELETE FROM public.user_saved_playlist');
+    db.query('UPDATE public.user SET ready=false');
+    db.query('DELETE FROM public.track_in_playlist');
+    db.query('DELETE FROM public.user_saved_playlist');
 });
 
 module.exports = {clearDb};
