bd90847af39e3ba00c9cea89d553338ebf118116
Maximo Machado <maximo@mit.edu>
2020-12-31 18:06:41 -0500

    Reorder tools in Tools page

--- client/src/pages/Tools/Tools.js
+++ client/src/pages/Tools/Tools.js
@@ -24,14 +24,14 @@ function Tools() {
                     path='/true-random-shuffle'
                 />
                 <Tool 
-                    title='Playlist Set Operations'
-                    description='This tool creates a new playlist based on two or more other playlists using set operations, i.e. an intersection would create a playlist containing only songs that are in both playlists.'
-                    path='/playlist-set-operations'
-                />
-                <Tool 
                     title='Multiple Playlist Searcher'
                     description='This tool looks through your saved playlists for a chosen song and tells you which playlists contain the song.'
                     path='/multiple-playlist-searcher'
+                />
+                <Tool 
+                    title='Playlist Set Operations'
+                    description='This tool creates a new playlist based on two or more other playlists using set operations, i.e. an intersection would create a playlist containing only songs that are in both playlists.'
+                    path='/playlist-set-operations'
                 />
             </StyledVStack>
         </PageLayout>
