2084ada18faeb87dd6151a23424b118448533c73
Maximo Machado <maximo@mit.edu>
2020-12-31 17:35:48 -0500

    Implement combinations and fix some bugs related to Symmetric Difference

--- server/utils/combinations.js
+++ server/utils/combinations.js
@@ -2,9 +2,28 @@
 function* combinations(array, length) {
     /**
      * Yields all combinations of a certain length from the array
+     * Params:
+     * array {array}: Array to take combinations of
+     * length {int}: Number of items in each combination
      */
 
-    
+    if (array.length === 0 || array.length < length || length === 0) {
+        yield [];
+    } else if (array.length === length) {
+        yield array.map(el => el);
+    } else {
+        rest = array.slice(1, array.length);
+
+        // Take first item
+        for (let combination of combinations(rest, length - 1)) {
+            yield [array[0]].concat(combination);
+        }
+
+        // Don't take first item
+        for (let combination of combinations(rest, length)) {
+            yield combination;
+        }
+    }
 }
 
 module.exports = {combinations};
